6e6f902278ac51fb28728d088e3272819a9b84d4
ssegane karim <drp6zjt2hy@privaterelay.appleid.com>
2026-09-21 09:36:40 +0300

    KARIM

---
+++ SSEGANE KARIM 2025_ITB_DAY_1948_G KABBYA/Kabbya Forex/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        lable2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        txtUGXYuan = New TextBox()
+        txtYuan = New TextBox()
+        txtYuanUGX = New TextBox()
+        txtUGXFromYuan = New TextBox()
+        txtUGXDirham = New TextBox()
+        txtDirham = New TextBox()
+        txtDirhamUGX = New TextBox()
+        txtUGXFromDirham = New TextBox()
+        Label8 = New Label()
+        Label9 = New Label()
+        btnToYuan = New Button()
+        btnYuanToUGX = New Button()
+        btnToDirham = New Button()
+        btnDirhamToUGX = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Comic Sans MS", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(485, 53)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(268, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "KABBYA FOREX BUREAU"
+        ' 
+        ' lable2
+        ' 
+        lable2.AutoSize = True
+        lable2.Location = New Point(334, 107)
+        lable2.Name = "lable2"
+        lable2.Size = New Size(30, 15)
+        lable2.TabIndex = 1
+        lable2.Text = "UGX"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(726, 107)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(89, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "CHINESE YUAN"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(334, 200)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(89, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "CHINESE YUAN"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(726, 200)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(30, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "UGX"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(334, 284)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(30, 15)
+        Label6.TabIndex = 5
+        Label6.Text = "UGX"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(726, 284)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(78, 15)
+        Label7.TabIndex = 6
+        Label7.Text = "UAE DIRHAM"
+        ' 
+        ' txtUGXYuan
+        ' 
+        txtUGXYuan.Location = New Point(334, 125)
+        txtUGXYuan.Name = "txtUGXYuan"
+        txtUGXYuan.Size = New Size(100, 23)
+        txtUGXYuan.TabIndex = 7
+        ' 
+        ' txtYuan
+        ' 
+        txtYuan.Location = New Point(708, 125)
+        txtYuan.Name = "txtYuan"
+        txtYuan.Size = New Size(100, 23)
+        txtYuan.TabIndex = 8
+        ' 
+        ' txtYuanUGX
+        ' 
+        txtYuanUGX.Location = New Point(334, 218)
+        txtYuanUGX.Name = "txtYuanUGX"
+        txtYuanUGX.Size = New Size(100, 23)
+        txtYuanUGX.TabIndex = 9
+        ' 
+        ' txtUGXFromYuan
+        ' 
+        txtUGXFromYuan.Location = New Point(708, 218)
+        txtUGXFromYuan.Name = "txtUGXFromYuan"
+        txtUGXFromYuan.Size = New Size(100, 23)
+        txtUGXFromYuan.TabIndex = 10
+        ' 
+        ' txtUGXDirham
+        ' 
+        txtUGXDirham.Location = New Point(334, 302)
+        txtUGXDirham.Name = "txtUGXDirham"
+        txtUGXDirham.Size = New Size(100, 23)
+        txtUGXDirham.TabIndex = 11
+        ' 
+        ' txtDirham
+        ' 
+        txtDirham.Location = New Point(708, 302)
+        txtDirham.Name = "txtDirham"
+        txtDirham.Size = New Size(100, 23)
+        txtDirham.TabIndex = 12
+        ' 
+        ' txtDirhamUGX
+        ' 
+        txtDirhamUGX.Location = New Point(334, 378)
+        txtDirhamUGX.Name = "txtDirhamUGX"
+        txtDirhamUGX.Size = New Size(100, 23)
+        txtDirhamUGX.TabIndex = 13
+        ' 
+        ' txtUGXFromDirham
+        ' 
+        txtUGXFromDirham.Location = New Point(708, 378)
+        txtUGXFromDirham.Name = "txtUGXFromDirham"
+        txtUGXFromDirham.Size = New Size(100, 23)
+        txtUGXFromDirham.TabIndex = 14
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(334, 360)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(78, 15)
+        Label8.TabIndex = 15
+        Label8.Text = "UAE DIRHAM"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(717, 360)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(30, 15)
+        Label9.TabIndex = 16
+        Label9.Text = "UGX"
+        ' 
+        ' btnToYuan
+        ' 
+        btnToYuan.Location = New Point(532, 124)
+        btnToYuan.Name = "btnToYuan"
+        btnToYuan.Size = New Size(135, 23)
+        btnToYuan.TabIndex = 17
+        btnToYuan.Text = "CONVERT TO YUAN"
+        btnToYuan.UseVisualStyleBackColor = True
+        ' 
+        ' btnYuanToUGX
+        ' 
+        btnYuanToUGX.Location = New Point(532, 218)
+        btnYuanToUGX.Name = "btnYuanToUGX"
+        btnYuanToUGX.Size = New Size(135, 23)
+        btnYuanToUGX.TabIndex = 18
+        btnYuanToUGX.Text = "CONVERT TO UGX"
+        btnYuanToUGX.UseVisualStyleBackColor = True
+        ' 
+        ' btnToDirham
+        ' 
+        btnToDirham.Location = New Point(532, 302)
+        btnToDirham.Name = "btnToDirham"
+        btnToDirham.Size = New Size(135, 23)
+        btnToDirham.TabIndex = 19
+        btnToDirham.Text = "CONVERT TO DIRHAM"
+        btnToDirham.UseVisualStyleBackColor = True
+        ' 
+        ' btnDirhamToUGX
+        ' 
+        btnDirhamToUGX.Location = New Point(532, 378)
+        btnDirhamToUGX.Name = "btnDirhamToUGX"
+        btnDirhamToUGX.Size = New Size(135, 23)
+        btnDirhamToUGX.TabIndex = 20
+        btnDirhamToUGX.Text = "CONVERT TO UGX"
+        btnDirhamToUGX.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.MenuHighlight
+        ClientSize = New Size(1091, 638)
+        Controls.Add(btnDirhamToUGX)
+        Controls.Add(btnToDirham)
+        Controls.Add(btnYuanToUGX)
+        Controls.Add(btnToYuan)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(txtUGXFromDirham)
+        Controls.Add(txtDirhamUGX)
+        Controls.Add(txtDirham)
+        Controls.Add(txtUGXDirham)
+        Controls.Add(txtUGXFromYuan)
+        Controls.Add(txtYuanUGX)
+        Controls.Add(txtYuan)
+        Controls.Add(txtUGXYuan)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(lable2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lable2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtUGXYuan As TextBox
+    Friend WithEvents txtYuan As TextBox
+    Friend WithEvents txtYuanUGX As TextBox
+    Friend WithEvents txtUGXFromYuan As TextBox
+    Friend WithEvents txtUGXDirham As TextBox
+    Friend WithEvents txtDirham As TextBox
+    Friend WithEvents txtDirhamUGX As TextBox
+    Friend WithEvents txtUGXFromDirham As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents btnToYuan As Button
+    Friend WithEvents btnYuanToUGX As Button
+    Friend WithEvents btnToDirham As Button
+    Friend WithEvents btnDirhamToUGX As Button
+
+End Class
---
+++ SSEGANE KARIM 2025_ITB_DAY_1948_G KABBYA/Kabbya Forex/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Private Sub btnToYuan_Click(sender As Object, e As EventArgs) Handles btnToYuan.Click
+
+
+        Dim ugx As Double
+        Dim yuan As Double
+
+        ugx = Val(txtUGXYuan.Text)
+
+        yuan = ugx / 540
+
+        txtYuan.Text = yuan.ToString()
+
+
+    End Sub
+
+    Private Sub btnYuanToUGX_Click(sender As Object, e As EventArgs) Handles btnYuanToUGX.Click
+
+
+        Dim yuan As Double
+        Dim ugx As Double
+
+        yuan = Val(txtYuanUGX.Text)
+
+        ugx = yuan * 540
+
+        txtUGXFromYuan.Text = ugx.ToString()
+
+
+    End Sub
+
+    Private Sub btnToDirham_Click(sender As Object, e As EventArgs) Handles btnToDirham.Click
+
+
+        Dim ugx As Double
+        Dim dirham As Double
+
+        ugx = Val(txtUGXDirham.Text)
+
+        dirham = ugx / 110
+
+        txtDirham.Text = dirham.ToString()
+
+
+    End Sub
+
+    Private Sub btnDirhamToUGX_Click(sender As Object, e As EventArgs) Handles btnDirhamToUGX.Click
+
+
+        Dim dirham As Double
+        Dim ugx As Double
+
+        dirham = Val(txtDirhamUGX.Text)
+
+        ugx = dirham * 110
+
+        txtUGXFromDirham.Text = ugx.ToString()
+
+
+    End Sub
+End Class
